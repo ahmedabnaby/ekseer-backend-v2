626b513adecd7fd3a78c4cc26c5cabf7b2369ce0
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2023-12-08 06:06:28 +0200

    added requirements.txt and gitignore file

--- ekseer_backend/settings.py
+++ ekseer_backend/settings.py
@@ -139,6 +139,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
